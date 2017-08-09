7c648449e33ab4b2665cdfbb40df236c19f9895c
Jaz Singh <jsingh@grailbio.com>
2017-08-09 09:14:51 -0700

    Add cors handler. Refactor api endpoints.

--- src/cors-handlers.js
+++ src/cors-handlers.js
@@ -1,12 +1,16 @@
 // Allow CORS access
 
-const optionsHandler = (app) => {
+const corsHandlers = (app) => {
 	app.options('/*', function(req, res) {
 		res.header('Access-Control-Allow-Origin', '*');
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 		res.sendStatus(200);
 	});
+	app.get('/*', function(req, res, next) {
+		res.header('Access-Control-Allow-Origin', '*');
+		next();
+	});
 };
 
-export {optionsHandler};
+export {corsHandlers};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {goLinkRoutes} from './go-links';
-import {optionsHandler} from './options';
+import {corsHandlers} from './cors-handlers';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,7 +10,7 @@ const config = {
 	port: 42666,
 };
 
-optionsHandler(app);
+corsHandlers(app);
 
 app.get('/', (req, res) => {
 	res.send('Hello world');
